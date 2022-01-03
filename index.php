549a53ad5a4fdc6e56955d3f290c738285b983d3
jaeblaezdacoder <jaeblaez325@gmail.com>
2022-01-03 09:41:00 -0500

    Add index.php page template to begin editting

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+#  Add your page title
+$title = "My Tyro-PHP Website";
+#  Add your page description
+$description = "A simple (but feature-rich!) PHP template script that allows you to create web pages without a database.";
+
+#  Display side menu
+#  Change to 1 to show side menu
+$showSide = 0;
+
+include ('header.php');
+?>
+
+<h1>Welcome!</h1>
+<div>Welcome to your website!<br><br></div>
+
+<div>You can modify this page by editing the index.php file.<br><br></div>
+
+<div>Visit our <a href="how-to.php">How To</a> guide for information on using this template.<br><br></div>
+
+<div>Use helper classes to make one, two or three column pages.  You can view the code on the Layout pages on our <a href="http://phpwebsitetemplates.com">website</a><br><br></div>
+
+<div class="one-half">
+	<h3>First Column</h3>
+	Nunc quis varius odio. In sed augue velit. Nunc vel auctor velit, vitae ultrices nunc. Nullam sed dui bibendum nunc luctus pharetra non eu nisl. Phasellus neque urna, semper at ultricies in, tempus tincidunt magna. Phasellus tempor posuere nulla, vel tincidunt dolor iaculis sodales. Suspendisse dapibus ipsum non nibh fermentum, quis gravida mauris tempor. Proin ultrices metus vitae congue vestibulum. Aenean et dui ac lorem consequat hendrerit. Suspendisse fermentum pellentesque velit sed lacinia. Curabitur ultrices purus lorem, mattis cursus ipsum cursus nec.
+</div>
+<div class="one-half">
+	<h3>Second Column</h3>
+	Nunc quis varius odio. In sed augue velit. Nunc vel auctor velit, vitae ultrices nunc. Nullam sed dui bibendum nunc luctus pharetra non eu nisl. Phasellus neque urna, semper at ultricies in, tempus tincidunt magna. Phasellus tempor posuere nulla, vel tincidunt dolor iaculis sodales. Suspendisse dapibus ipsum non nibh fermentum, quis gravida mauris tempor. Proin ultrices metus vitae congue vestibulum. Aenean et dui ac lorem consequat hendrerit. Suspendisse fermentum pellentesque velit sed lacinia. Curabitur ultrices purus lorem, mattis cursus ipsum cursus nec.
+</div>
+
+<div class="clear">
+</div>
+
+
+<?php
+include ('footer.php');
+?>
